aa446203635569a2b0d0637805456e20787c56c1
ayush <ayush@AYUSHGANDHI>
2024-01-29 17:47:31 +0530

    Add project files.

---
+++ table of 5 in loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table_of_5_in_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i = i + 1) 
+            {
+                Console.WriteLine(i*5);
+                Console.ReadKey();
+            }
+        }
+    }
+}
